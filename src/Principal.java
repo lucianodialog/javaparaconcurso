886185ab567113570213a61eb7a99461b91d92b4
Luciano Silva <luciano_dialog@yahoo.com.br>
2019-06-16 13:38:49 -0300

    Inicio do Projeto

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,8 @@
-
+/*
+ * Criado por Luciano Alexandre da Silva.
+ * Objetivo: Estudar java para concursos.
+ * Criado em: 16/06/2019.
+ * */
 public class Principal {
 
 	public static void main(String[] args) {
