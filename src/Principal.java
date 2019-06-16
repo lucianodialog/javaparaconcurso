f6d08bf592f95587016676e23114a19a75ebb787
Luciano Silva <luciano_dialog@yahoo.com.br>
2019-06-16 15:33:21 -0300

    Enum aula55 Loiane metodos value() e valueOf()

--- src/Principal.java
+++ src/Principal.java
@@ -8,12 +8,15 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-        DiaSemana dia = DiaSemana.SEGUNDA;
-		
-		System.out.println(dia.toString() + " - " + dia.getValor());
+		DiaSemana[] dias = DiaSemana.values();
 
+		for (int i=0; i<dias.length; i++){
+			System.out.println(dias[i]);
+		}
 		
-		Data data = new Data(1, 4, 2016, DiaSemana.SEXTA);
+		for (DiaSemana dia : DiaSemana.values()){
+			System.out.println(dia);
+		}
 
 	}
 	
